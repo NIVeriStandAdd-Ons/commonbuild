d3d737e03b393be497477a0ee1aef4b9099d58e9
Andy Church <church.andrew.d@gmail.com>
2018-03-01 14:49:17 -0600

    moving cloneCommonbuildConfiguration.groovy to config library

--- vars/cloneCommonbuildConfiguration.groovy
+++
@@ -1,8 +0,0 @@
-def call(){
-   echo 'Cloning commonbuild configuration to workspace.'
-   
-   def organization = getComponentParts()['organization']
-   def branch = env."library.vs-common-build-configuration.version"
-   commonbuildConfigDir = cloneRepo("https://github.com/$organization/commonbuild-configuration", branch)
-   return commonbuildConfigDir
-}
