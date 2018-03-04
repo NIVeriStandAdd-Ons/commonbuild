b495dc0a64599bfff7f03260e0aed7867504c0b9
Andy Church <church.andrew.d@gmail.com>
2018-03-04 12:51:13 -0600

    trying different syntax

--- src/ni/vsbuild/packages/Nipkg.groovy
+++ src/ni/vsbuild/packages/Nipkg.groovy
@@ -33,10 +33,10 @@ class Nipkg extends AbstractPackage {
       def repo = script.getComponentParts()['repo']
       def branch = script.getComponentParts()['branch']
 
-      def componentID = "$repo-$branch"
+      def componentID = '$repo-$branch'
       script.echo "Getting build version number for ${componentID}."
       
-      def versionConfig = convertedVersionConfigJson.repositories.get('$repo-$branch')
+      def versionConfig = convertedVersionConfigJson.repositories.get(componentID)
       script.echo "$versionConfig"
       //def buildNumber = versionConfig.get('build')
       //script.echo "$buildNumber"
