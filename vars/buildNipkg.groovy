7e6c6a547b258d7837a0338b5c35de38d5ee45cf
Andy Church <church.andrew.d@gmail.com>
2018-03-05 21:12:20 -0600

    debugging

--- vars/buildNipkg.groovy
+++ vars/buildNipkg.groovy
@@ -4,16 +4,16 @@ def call(payloadDir, buildNumber, stagingPath, devXmlPath, lvVersion) {
    def basePackageName = "${controlFields.get('Package')}"
    def nipmAppPath = "C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
    def controlFileText = readFile "control" // Read nipkg control file 
-
+/*
    def devXmlText = readFile devXmlPath
    def devXml = new XmlSlurper().parseText(devXmlText)
    def baseVersion = devXml.Version.text()
    def nipkgVersion = "${baseVersion}.${buildNumber}"
    echo "nipkg Version: $nipkgVersion"
-
+*/
    // Replace {version} with current VeriStand version being built.
    def newControlFileText = controlFileText.replaceAll("\\{veristand_version\\}", "${lvVersion}")
-   finalControlFileText = newControlFileText.replaceAll("\\{nipkg_version\\}", "${nipkgVersion}")
+//   finalControlFileText = newControlFileText.replaceAll("\\{nipkg_version\\}", "${nipkgVersion}")
    def newStagingPath = stagingPath.replaceAll("\\{veristand_version\\}", "${lvVersion}")
    def packageName = basePackageName.replaceAll("\\{veristand_version\\}", "${lvVersion}")
    echo newStagingPath
@@ -22,7 +22,7 @@ def call(payloadDir, buildNumber, stagingPath, devXmlPath, lvVersion) {
    echo finalControlFileText
 
    // Copy package payload to nipkg staging directory. 
-//   bat "(robocopy \"${payloadDir}\" \"nipkg\\${packageName}\\data\\${newStagingPath}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
+   bat "(robocopy \"${payloadDir}\" \"nipkg\\${packageName}\\data\\${newStagingPath}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
 
 //   writeFile file: "nipkg\\${packageName}\\debian-binary", text: "2.0"      
 //   writeFile file: "nipkg\\${packageName}\\control\\control", text: finalControlFileText
