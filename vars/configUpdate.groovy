3178839cdc5d39ff797e08264ffe0f4424b05f6f
Andy Church <church.andrew.d@gmail.com>
2018-03-06 17:09:15 -0600

    adding configUpdate script

---
+++ vars/configUpdate.groovy
@@ -0,0 +1,20 @@
+def call(updatedComponent, configuration) {
+
+   def commitMessage = "updating $updatedComponent."
+   echo commitMessage
+
+   def configurationTOMLFile = new File("${WORKSPACE}\\commonbuild-configuration\\configuration.toml")
+   def configurationTOML = configurationTOMLFile.newWriter()
+   
+   configuration.repositories.each { repository, properties ->
+      echo "Repository: $repository Properties: $properties"
+      configurationTOML << "[repositories.$repository]\n"
+      properties.each { property, value ->
+         configurationTOML << "${property} = '${value}'\n"
+      }
+      configurationTOML << "\n"
+   }
+   
+   configurationTOML.flush()
+   configurationTOML.close()
+}
