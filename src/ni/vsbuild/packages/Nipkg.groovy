9f4de95b005d49992c3f7c8c3f93daf7853aaa2e
Andy Church <church.andrew.d@gmail.com>
2018-03-07 22:46:25 -0600

    changed tag string to include year and branch

--- src/ni/vsbuild/packages/Nipkg.groovy
+++ src/ni/vsbuild/packages/Nipkg.groovy
@@ -49,8 +49,8 @@ class Nipkg extends AbstractPackage {
 
       // Update the configuration map, save it to disk, and push to github.com\{your_org}\commonbuild-configuration. 
       script.configUpdate(buildNumber, buildID, componentID, configurationMap)
+      script.pushRelease(nipkgName, baseVersion, buildNumber, payloadDir, lvVersion)
       script.configPush(buildNumber, componentID, lvVersion) 
-      script.pushRelease(nipkgName, baseVersion, buildNumber, payloadDir)
    }
 }
 
--- vars/pushRelease.groovy
+++ vars/pushRelease.groovy
@@ -1,21 +1,21 @@
-def call(packageName, baseVersion, buildNumber, payloadDir) {
+def call(packageName, baseVersion, buildNumber, payloadDir, lvVersion) {
 
    // Add all files in payloadDir to a GitHub release.
    
    def branch = getComponentParts()['branch']
    def org = getComponentParts()['organization']
    def repo = getComponentParts()['repo']
-   def nipkgVersion = "${baseVersion}.${buildNumber}"
-   def releaseName = "${packageName}_${nipkgVersion}"
+   def tagString = "${lvVersion}-${baseVersion}.${buildNumber}-$branch"
+   def releaseName = "${packageName}_${tagString}"
    def description = "$releaseName built from branch $branch."
    def nipkgPath = "${payloadDir}\\${releaseName}_windows_x64.nipkg"
 
    if(branch == 'master') {
-      bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $nipkgVersion --description \"${description}\""
+      bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $tagString --description \"${description}\""
    }
    if(branch == 'develop') {
-      bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $nipkgVersion --description \"${description}\" --pre-release"
+      bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $tagString --description \"${description}\" --pre-release"
    }
    
-   bat "github-release upload --user $org --repo $repo --name \"${releaseName}_windows_x64.nipkg\" --tag $nipkgVersion --file \"${nipkgPath}\""
+   bat "github-release upload --user $org --repo $repo --name \"${releaseName}_windows_x64.nipkg\" --tag $tagString --file \"${nipkgPath}\""
 }
