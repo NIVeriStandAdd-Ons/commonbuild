ecb602477261cc5c765a7a1439eb29470cbecc57
Andy Church <church.andrew.d@gmail.com>
2018-03-07 20:32:51 -0600

    using map.containsKey to check for config entries

--- vars/getBuildNumber.groovy
+++ vars/getBuildNumber.groovy
@@ -3,13 +3,15 @@ def call(buildID, componentID, configurationMap) {
    def buildNumber = 0
 
    // Read last stored build number from configurationMap. If a value exists, increment it. 
-   def componentConfig = configurationMap.repositories.get(componentID)
-   if(componentConfig) {
-	   def lastBuild = componentConfig.get(buildID) as Integer
-	   if(lastBuild) {
-		  buildNumber = lastBuild + 1
-	   }
+   if(configurationMap.repositories.containsKey(componentID)) {
+      def componentConfig = configurationMap.repositories.get(componentID)
+	  if(componentConfig.containsKey(buildID)) {
+	     def lastBuild = componentConfig.get(buildID) as Integer
+		 buildNumber = lastBuild + 1
+	  }
 	   return buildNumber
+
+   //  If the component doesn't exist, add it to the map. 
    } else {
       configurationMap.repositories[componentID] = [buildID: buildNumber]
       return buildNumber
