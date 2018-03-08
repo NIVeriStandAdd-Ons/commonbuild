670d59c59df6779216fe9bca29a4103a6d0f38be
Andy Church <church.andrew.d@gmail.com>
2018-03-07 21:43:00 -0600

    update release parameters

--- vars/pushRelease.groovy
+++ vars/pushRelease.groovy
@@ -7,8 +7,9 @@ def call(packageName, baseVersion, buildNumber, payloadDir) {
    def repo = getComponentParts()['repo']
    def versionTag = "${baseVersion}.${buildNumber}"
    def releaseName = "${packageName}-${versionTag}"
+   def description = "$releaseName built from branch $branch."
 
    if(branch == 'master') {
-      bat "github-release release --user $org --repo $repo --name $packageName --tag $baseVersion --description test"
+      bat "github-release release --user $org --repo $repo --name $releaseName --tag $versionTag --description $description"
    }
 }
