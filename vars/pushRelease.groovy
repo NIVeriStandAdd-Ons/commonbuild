522b994f76a8e8f8fc7cd798545cc8469b99dac8
Andy Church <church.andrew.d@gmail.com>
2018-03-07 21:38:12 -0600

    update tag and release name

--- vars/pushRelease.groovy
+++ vars/pushRelease.groovy
@@ -5,6 +5,8 @@ def call(packageName, baseVersion, buildNumber, payloadDir) {
    def branch = getComponentParts()['branch']
    def org = getComponentParts()['organization']
    def repo = getComponentParts()['repo']
+   def versionTag = "${baseVersion}.${buildNumber}"
+   def releaseName = "${packageName}-${versionTag}"
 
    if(branch == 'master') {
       bat "github-release release --user $org --repo $repo --name $packageName --tag $baseVersion --description test"
