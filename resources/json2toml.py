aafb4bd12ddd9f3efd71076738a12ed608141de2
Andy Church <church.andrew.d@gmail.com>
2018-03-10 16:52:53 -0600

    load file instead of string

--- resources/json2toml.py
+++ resources/json2toml.py
@@ -5,7 +5,7 @@ import toml
 from os.path import split, splitext
 
 file = sys.argv[1]
-parsed_json = json.loads(open(file))
+parsed_json = json.load(file)
 
 base_path, file_name = os.path.split(file)
 toml_file_name = os.path.splitext(file_name)[0] + '.toml'
