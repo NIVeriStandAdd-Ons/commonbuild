85cd6f8dc07175ea796860b8b3c24cf42372d08a
Andy Church <church.andrew.d@gmail.com>
2018-03-10 14:50:33 -0600

    added debug and else statement to getReleaseInfo script

--- vars/getReleaseInfo.groovy
+++ vars/getReleaseInfo.groovy
@@ -6,10 +6,13 @@ def call(componentConfig, lvVersion) {
    // Read the release branch info from configuration.json. The list of branches determines whether to push releases or not.
    if(componentConfig.containsKey(releaseBranchInfoKey)) {
       releaseBranches = componentConfig.get(releaseBranchInfoKey)
+      echo "Branches configured for release: $releaseBranches"
       return releaseBranches
-   }
+   } else {
+      echo "No branches configured for GitHub releases."
    // Return null if the key isn't found: don't push any releases.
-   return null
+      return null
+   }
 }
 
    
