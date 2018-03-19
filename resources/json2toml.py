7dae785503a6c7bf25feadeac8c0aca39df92931
Andy Church <church.andrew.d@gmail.com>
2018-03-19 12:54:19 -0500

    move updated configuration.toml into commonbuild-configuration

--- resources/json2toml.py
+++ resources/json2toml.py
@@ -9,7 +9,7 @@ fileObj = open(file)
 parsed_json = json.load(fileObj)
 
 base_path, file_name = os.path.split(file)
-toml_file_name = os.path.splitext(file_name)[0] + '.toml'
+toml_file_name = 'commonbuild-configuration\\' + os.path.splitext(file_name)[0] + '.toml'
 
 print("Writing build description to", toml_file_name)
 with open(toml_file_name, 'w') as outfile:
