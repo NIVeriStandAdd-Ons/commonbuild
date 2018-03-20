7f2180055178dc8fdede69f3a9ed316cc939aff3
Andy Church <church.andrew.d@gmail.com>
2018-03-20 00:25:40 -0500

    move git commands to batch file

---
+++ resources/configPush.bat
@@ -0,0 +1,11 @@
+@echo off
+
+SET commitMessage= "%1"
+
+CD commonbuild-configuration
+
+git commit -a -m %commitMessage%
+git pull origin master
+git push --set-upstream origin master
+
+@echo on
--- vars/configPush.groovy
+++ vars/configPush.groovy
@@ -3,7 +3,5 @@ def call(buildNumber, componentID, lvVersion) {
    // Push the updated build configuration back to GitHub configuration repository. 
    echo "Updating build number for ${componentID} (${lvVersion}) to ${buildNumber} in commonbuild-configuration repository."
    def commitMessage = "Updating ${componentID} for VeriStand ${lvVersion} to build number ${buildNumber}."
-   bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git commit -a -m \"$commitMessage\""
-   bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git pull origin master"
-   bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git push --set-upstream origin master"
+   bat "commonbuild\\resources\\configPush.bat $commitMessage"
 }
