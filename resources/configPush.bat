c87c834e66d172dd37da5f924d16448796d7f0d0
Andy Church <church.andrew.d@gmail.com>
2018-03-20 09:08:18 -0500

    adding delay before pulling again on fail

--- resources/configPush.bat
+++ resources/configPush.bat
@@ -1,11 +1,20 @@
-@echo off
+@echo on
 
 SET commitMessage=%1
 
 CD commonbuild-configuration
 
 git commit -a -m %commitMessage%
+
+:Wait
+TIMEOUT /T 10
 git pull origin master
-git push --set-upstream origin master
+
+git push --set-upstream origin master && (
+   echo git push was successful
+) || (
+   echo trying git pull again after wait
+   GOTO Wait
+)
 
 @echo on
