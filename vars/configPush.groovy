e38c6ead8a7701ac705dc6a8155a9c6a18da4f76
Andy Church <church.andrew.d@gmail.com>
2018-03-20 00:05:27 -0500

    add git pull to grab changes from other build nodes

--- vars/configPush.groovy
+++ vars/configPush.groovy
@@ -4,5 +4,6 @@ def call(buildNumber, componentID, lvVersion) {
    echo "Updating build number for ${componentID} (${lvVersion}) to ${buildNumber} in commonbuild-configuration repository."
    def commitMessage = "Updating ${componentID} for VeriStand ${lvVersion} to build number ${buildNumber}."
    bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git commit -a -m \"$commitMessage\""
+   bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git pull origin master"
    bat "git --work-tree=commonbuild-configuration --git-dir=commonbuild-configuration\\.git push --set-upstream origin master"
 }
