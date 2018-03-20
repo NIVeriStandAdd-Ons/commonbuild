89a1900f4389893c89793846081cf1452974f0a8
Andy Church <church.andrew.d@gmail.com>
2018-03-19 22:30:19 -0500

    explicitly create JSONObject

--- vars/configUpdate.groovy
+++ vars/configUpdate.groovy
@@ -1,7 +1,8 @@
 def call(configurationMap, lvVersion) {
 
    def configurationJsonFileName = "configuration_${lvVersion}.json"   
-   def configurationJSON = readJSON text: '$configurationMap'
+   def configurationJSON = new.net.sf.json.JSONObject()
+   configurationJSON.putAll(configurationMap)
 
    // Write configuration to JSON file and then convert it back to TOML. 
    writeJSON file: configurationJsonFileName, json: configurationJSON, pretty: 4
