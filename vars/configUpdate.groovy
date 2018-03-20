ef0dc96202f8ffe1bab291c26a999726fd3c923a
Andy Church <church.andrew.d@gmail.com>
2018-03-19 22:20:10 -0500

    quotes

--- vars/configUpdate.groovy
+++ vars/configUpdate.groovy
@@ -1,7 +1,7 @@
 def call(configurationMap, lvVersion) {
 
    def configurationJsonFileName = "configuration_${lvVersion}.json"   
-   def configurationJSON = readJSON text: configurationMap
+   def configurationJSON = readJSON text: '$configurationMap'
 
    // Write configuration to JSON file and then convert it back to TOML. 
    writeJSON file: configurationJsonFileName, json: configurationJSON, pretty: 4
