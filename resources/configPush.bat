3c794e07655b11063d178186ef28e8be19ade9e4
Andy Church <church.andrew.d@gmail.com>
2018-03-20 02:14:18 -0500

    remove quotes

--- resources/configPush.bat
+++ resources/configPush.bat
@@ -1,10 +1,10 @@
 @echo off
 
-SET commitMessage= "%1"
+SET commitMessage=%1
 
 CD commonbuild-configuration
 
-git commit -a -m "%commitMessage%"
+git commit -m -a %commitMessage%
 git pull origin master
 git push --set-upstream origin master
 
