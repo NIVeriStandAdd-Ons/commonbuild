6b75f04789efe3b72f18bbb5864454870af374e9
Andy Church <church.andrew.d@gmail.com>
2018-03-19 21:16:43 -0500

    read year version specific config file

--- resources/buildSetup.bat
+++ resources/buildSetup.bat
@@ -9,7 +9,7 @@ call %venv_path%\\Scripts\\activate.bat
 
 pip install toml
 python commonbuild\\resources\\toml2json.py build.toml
-python commonbuild\\resources\\toml2json.py commonbuild-configuration\\configuration.toml
+python commonbuild\\resources\\toml2json.py %1
 
 call %venv_path%\\Scripts\\deactivate.bat
 
