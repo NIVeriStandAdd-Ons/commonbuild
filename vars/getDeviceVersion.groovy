faedf824006ee3f95982b955b8f02c9692fa655c
Andy Church <church.andrew.d@gmail.com>
2018-03-29 16:09:07 -0500

    add debug to XML parsing; trim XML string

--- vars/getDeviceVersion.groovy
+++ vars/getDeviceVersion.groovy
@@ -1,5 +1,7 @@
 def call(devXmlPath) {
    def devXmlText = readFile devXmlPath
+   echo "$devXmlText"
+   devXmlText = devXmlText.trim().replaceFirst("^([\\W]+)<","<")
    def devXml = new XmlSlurper().parseText(devXmlText)
    def baseVersion = devXml.Version.text()
 
