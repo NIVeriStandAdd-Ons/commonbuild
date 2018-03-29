283cb1add4df3a571f0525b3074ffb17cda88a45
Andy Church <church.andrew.d@gmail.com>
2018-03-29 17:08:47 -0500

    remove echo

--- vars/getDeviceVersion.groovy
+++ vars/getDeviceVersion.groovy
@@ -1,6 +1,5 @@
 def call(devXmlPath) {
    def devXmlText = readFile devXmlPath
-   echo "$devXmlText"
    def devXml = new XmlSlurper().parseText(devXmlText)
    def baseVersion = devXml.Version.text()
 
