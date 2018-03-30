03966e8d441020bfc6b1ef1bb043b80371b7a302
Andy Church <church.andrew.d@gmail.com>
2018-03-30 09:23:44 -0500

    add echo again

--- vars/getDeviceVersion.groovy
+++ vars/getDeviceVersion.groovy
@@ -1,5 +1,6 @@
 def call(devXmlPath) {
    def devXmlText = readFile devXmlPath
+   echo "$devXmlText"
    def devXml = new XmlSlurper().parseText(devXmlText)
    def baseVersion = devXml.Version.text()
 
