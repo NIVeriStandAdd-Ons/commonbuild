8fcf09b8801530e1398f21c8ec96f8670bb17e76
Andy Church <church.andrew.d@gmail.com>
2018-03-30 10:33:10 -0500

    move XML echo after trim

--- vars/getDeviceVersion.groovy
+++ vars/getDeviceVersion.groovy
@@ -1,7 +1,7 @@
 def call(devXmlPath) {
    def devXmlText = readFile devXmlPath
-   echo "$devXmlText"
    devXmlText = devXmlText.trim().replaceFirst("^([\\W]+)<","<")
+   echo "$devXmlText"
    def devXml = new XmlSlurper().parseText(devXmlText)
    def baseVersion = devXml.Version.text()
 
