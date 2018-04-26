bbed7ac2db74c749f212d899253cdbd13e3e5ea3
Andy Church <church.andrew.d@gmail.com>
2018-04-26 15:09:53 -0500

    install .nipkg at beginning of integration test

--- vars/runIntegrationTest.groovy
+++ vars/runIntegrationTest.groovy
@@ -1,7 +1,10 @@
 def call(seqPath, tsVersion) {
    echo "Running test $seqPath with TestStand $tsVersion"
+   def nipmAppPath = "C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
    def seqEditorPath = "C:\\Program Files (x86)\\National Instruments\\TestStand ${tsVersion}\\Bin\\SeqEdit.exe"
    def sequencePath = "${WORKSPACE}\\${seqPath}"
+   def nipkgFilePath = readFile file: "build_log"
 
+   bat "\"${nipmAppPath}\" -y \"${nipkgFilePath}\""
    bat "\"${seqEditorPath}\" /outputToStdIO /run MainSequence \"${sequencePath}\" /quit"
 }
