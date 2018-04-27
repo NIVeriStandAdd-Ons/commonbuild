52ce99ab3b9314e2fe569a5dc2d386926298da70
Andy Church <church.andrew.d@gmail.com>
2018-04-27 11:35:52 -0500

    add lvVersion

--- src/ni/vsbuild/packages/Nipkg.groovy
+++ src/ni/vsbuild/packages/Nipkg.groovy
@@ -60,7 +60,7 @@ class Nipkg extends AbstractPackage {
 
       // Update the configuration map, save it to disk, and push to github.com\{your_org}\commonbuild-configuration. 
       script.configUpdate(configurationJSON, lvVersion)
-      script.vipmGetInstalled()
+      script.vipmGetInstalled(lvVersion)
       releaseBranches = script.getReleaseInfo(componentName, configurationMap, lvVersion)
       script.configPush(buildNumber, componentName, lvVersion) 
       script.pushRelease(nipkgInfo, payloadDir, releaseBranches, lvVersion)
--- vars/vipmGetInstalled.groovy
+++ vars/vipmGetInstalled.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(lvVersion){
    echo "Getting list of installed VI Packages."
    bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\commonbuild\\lv\\vipm\\vipmGetInstalled.vi\" -- \"$WORKSPACE\""
 }
