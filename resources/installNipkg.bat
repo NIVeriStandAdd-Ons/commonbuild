c694acca66351dc9d8c5e8808fb998438c4e3715
Andy Church <church.andrew.d@gmail.com>
2018-04-27 10:33:45 -0500

    add quotes

--- resources/installNipkg.bat
+++ resources/installNipkg.bat
@@ -4,4 +4,4 @@ SET nipkgFilePath=%3
 
 SET nipmAppPath="C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
 
-%nipmAppPath% install -y %nipkgFilePath%
+"%nipmAppPath% install -y %nipkgFilePath%"
