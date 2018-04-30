876356d0a0fc57cb16ab35bd09432e20beacb6a5
Andy Church <church.andrew.d@gmail.com>
2018-04-30 10:18:35 -0500

    add nipmGetInstalled() script

--- src/ni/vsbuild/packages/Nipkg.groovy
+++ src/ni/vsbuild/packages/Nipkg.groovy
@@ -61,6 +61,7 @@ class Nipkg extends AbstractPackage {
       // Update the configuration map, save it to disk, and push to github.com\{your_org}\commonbuild-configuration. 
       script.configUpdate(configurationJSON, lvVersion)
       script.vipmGetInstalled(lvVersion)
+      script.nipmGetInstalled()
       releaseBranches = script.getReleaseInfo(componentName, configurationMap, lvVersion)
       script.configPush(buildNumber, componentName, lvVersion) 
       script.pushRelease(nipkgInfo, payloadDir, releaseBranches, lvVersion)
---
+++ vars/nipmGetInstalled.groovy
@@ -0,0 +1,6 @@
+def call(){
+   echo "Getting list of installed NI Packages."
+   def nipmAppPath = "C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
+
+   bat "\"${nipmAppPath}\" list-installed >> build_log"
+}
