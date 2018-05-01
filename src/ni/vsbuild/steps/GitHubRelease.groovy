5c3c4bc2d1f481d151670b80c63b0cdd49310df4
Andy Church <church.andrew.d@gmail.com>
2018-04-30 22:53:42 -0500

    add release to githubRelease(); add lvVersion variable to GitHubRelease

--- src/ni/vsbuild/steps/GitHubRelease.groovy
+++ src/ni/vsbuild/steps/GitHubRelease.groovy
@@ -5,13 +5,15 @@ import ni.vsbuild.BuildConfiguration
 class GitHubRelease extends AbstractStep {
    
    def releaseBranches
+   def lvVersion
    
    GitHubRelease(script, mapStep, lvVersion) {
       super(script, mapStep)
+      this.lvVersion = lvVersion
       this.releaseBranches = mapStep.get('release_branches')
    }
 
    void executeStep(BuildConfiguration buildConfiguration) {
-      script.githubRelease(releaseBranches)
+      script.githubRelease(releaseBranches, lvVersion)
    }
 }
--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -1,5 +1,31 @@
-def call(releaseBranches) {
+def call(releaseBranches, lvVersion) {
 
    echo "Test release stage: $releaseBranches"
 
+   def repo = getComponentParts()['repo']
+   def branch = getComponentParts()['branch']
+   def org = getComponentParts()['org']
+
+   def buildLog = readProperties file: "build_log"
+   def nipkgVersion = buildLog.get('Package Version')
+   def nipkgName = buildLog.get('Package Name')
+   def nipkgPath = buildLog.get('Package File Path')
+   
+   def tagString = "${lvVersion}-${nipkgVersion}"
+   def releaseName = "${nipkgName}_${nipkgVersion}"
+   def description = "$releaseName built from branch $branch."
+
+   if(releaseBranches != null && releaseBranches.contains(branch)) {
+      echo "Releasing branch \'${branch}\' at www.github.com\${org}\${repo}."
+      if(branch == 'master') {
+         bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $tagString --description \"${description}\""
+      } else {
+         bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $tagString --description \"${description}\" --pre-release"
+      }
+      bat "github-release upload --user $org --repo $repo --name \"${releaseName}_windows_x64.nipkg\" --tag $tagString --file version_manifest"
+      bat "github-release upload --user $org --repo $repo --name \"${releaseName}_windows_x64.nipkg\" --tag $tagString --file \"${nipkgPath}\""
+   }
+   else {
+      echo "Branch \'${branch}\' is not configured for release."
+   }
 }
