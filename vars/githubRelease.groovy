d32e24b359787e3aa06650c965849b62a4ce1e49
Andy Church <church.andrew.d@gmail.com>
2018-05-02 08:48:10 -0500

    check for globally configured release branches

--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -1,3 +1,6 @@
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonOutput
+
 def call(releaseBranches, lvVersion) {
 
    echo "Test release stage: $releaseBranches"
@@ -5,6 +8,7 @@ def call(releaseBranches, lvVersion) {
    def repo = getComponentParts()['repo']
    def branch = getComponentParts()['branch']
    def org = getComponentParts()['organization']
+   def globalReleaseBranches
 
    def buildLog = readProperties file: "build_log"
    def nipkgVersion = buildLog.get('PackageVersion')
@@ -17,7 +21,18 @@ def call(releaseBranches, lvVersion) {
    def releaseName = "${nipkgName}_${nipkgVersion}"
    def description = "$releaseName built from branch $branch."
 
-   if(releaseBranches != null && releaseBranches.contains(branch)) {
+   configurationJsonFile = readJSON file: "configuration_${lvVersion}.json"
+   configurationMap = new JsonSlurperClassic().parseText(configurationJsonFile.toString())
+   def componentConfiguration = configurationMap.repositories.get(repo)
+
+   if(componentConfiguration.containsKey('release_branches')) {
+      globalReleaseBranches = componentConfiguration.get('release_branches')
+      echo "Branches configured globally in commonbuild-configuration for release: $globalReleaseBranches"
+   } else {
+      echo "No branches configured globally in commonbuild-configuration for GitHub releases."
+   }
+
+   if(releaseBranches != null && releaseBranches.contains(branch) || (globalReleaseBranches != null && globalReleaseBranches.contains(branch))) {
       echo "Releasing branch \'${branch}\' at www.github.com\${org}\${repo}."
       if(branch == 'master') {
          bat "github-release release --user $org --repo $repo --target $branch --name $releaseName --tag $tagString --description \"${description}\""
