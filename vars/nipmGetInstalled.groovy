73b2b8ff7c566f458c61f378baa45b631aa0ccc5
Andy Church <church.andrew.d@gmail.com>
2018-05-03 17:02:32 -0500

    store version info in build_log

--- vars/nipmGetInstalled.groovy
+++ vars/nipmGetInstalled.groovy
@@ -1,6 +1,15 @@
 def call(){
-   echo "Getting list of installed NI Packages."
+
+   echo "Fetching list of installed NI Packages."
+
+   //Use NI Package Managet CLI to get list of installed NI Packages.
    def nipmAppPath = "C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
-   bat "echo \"NI Package Versions installed:\" >version_manifest"
-   bat "\"${nipmAppPath}\" list-installed >version_manifest"
+   bat "echo \"NI Package Versions installed:\" >nipm_version_manifest"
+   bat "\"${nipmAppPath}\" list-installed >nipm_version_manifest"
+   def installedNipkgList = readFile "nipm_version_manifest"
+
+   //Add NIPM output to build log.
+   def buildLog = new File('build_log')
+   buildLog.append(installedNipkgList)
+
 }
