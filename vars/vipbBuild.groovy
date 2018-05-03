57d485089ba4525c59f865eb627ee6af26b3b56f
Andy Church <church.andrew.d@gmail.com>
2018-05-03 13:35:16 -0500

    escape chars

--- vars/vipbBuild.groovy
+++ vars/vipbBuild.groovy
@@ -1,4 +1,4 @@
 def call(vipbPath, outputPath, lvVersion) {
-        echo "Build the package at ${path}"
-        bat "labview-cli --kill commonbuild\lv\vipm\vipbBuild.vi -- \"${vipbPath}\" \"${outputPath}\" \"${WORKSPACE}\""  
+        echo "Build the package at ${vipbPath}"
+        bat "labview-cli --kill commonbuild\\lv\\vipm\\vipbBuild.vi -- \"${vipbPath}\" \"${outputPath}\" \"${WORKSPACE}\""  
 }
