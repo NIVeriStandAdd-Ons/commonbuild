639a4d88c4e67c3817f571bc399ec9c5aee2845d
Andy Church <church.andrew.d@gmail.com>
2018-05-04 11:36:11 -0500

    update variable names

--- vars/integrationTest.groovy
+++ vars/integrationTest.groovy
@@ -2,11 +2,11 @@ def call(seqPath, tsVersion) {
    echo "Running test $seqPath with TestStand $tsVersion"
    def seqEditorPath = "C:\\Program Files (x86)\\National Instruments\\TestStand ${tsVersion}\\Bin\\SeqEdit.exe"
    def sequencePath = "${WORKSPACE}\\${seqPath}"
-   def buildLog = readProperties file: "build_log"
-   def nipkgFileLoc = buildLog.get('PackageFileLoc')
-   def nipkgFileName = buildLog.get('PackageFileName')
-   def nipkgFilePath = "$nipkgFileLoc\\$nipkgFileName"
+   def buildLog = readProperties file: "build_properties"
+   def packageFileLoc = buildLog.get('PackageFileLoc')
+   def packageFileName = buildLog.get('PackageFileName')
+   def packageFilePath = "$packageFileLoc\\$packageFileName"
 
-   bat "C:\\github-release\\elevate-1.3.0-x86-64\\elevate.exe -k commonbuild\\resources\\installNipkg.bat \"${nipkgFilePath}\""
+   bat "C:\\github-release\\elevate-1.3.0-x86-64\\elevate.exe -k commonbuild\\resources\\installNipkg.bat \"${packageFilePath}\""
    bat "\"${seqEditorPath}\" /outputToStdIO /run MainSequence \"${sequencePath}\" /quit"
 }
