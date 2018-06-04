3a182b4e162b0852af6c8c0c7271395d33cf3c7d
Andy Church <church.andrew.d@gmail.com>
2018-06-04 11:27:45 -0500

    use TestStand version selector before integration test

--- vars/integrationTest.groovy
+++ vars/integrationTest.groovy
@@ -1,12 +1,17 @@
 def call(seqPath, tsVersion) {
    echo "Running test $seqPath with TestStand $tsVersion"
+
    def seqEditorPath = "C:\\Program Files (x86)\\National Instruments\\TestStand ${tsVersion}\\Bin\\SeqEdit.exe"
+   def tsVersionSelectorPath = "C:\\Program Files (x86)\\National Instruments\\Shared\\TestStand Version Selector\\TSVerSelect.exe"
    def sequencePath = "${WORKSPACE}\\${seqPath}"
    def buildLog = readProperties file: "build_properties"
    def packageFileLoc = buildLog.get('PackageFileLoc')
    def packageFileName = buildLog.get('PackageFileName')
    def packageFilePath = "$packageFileLoc\\$packageFileName"
 
+   formattedTSVersion = tsVersion.substring(2,4)+".0"
+
    bat "C:\\github-release\\elevate-1.3.0-x86-64\\elevate.exe -k commonbuild\\resources\\installNipkg.bat \"${packageFilePath}\""
+   bat "\"${tsVersionSelectorPath}\" /${tsVersion} ${formattedTSVersion} /installing /noprompt"
    bat "\"${seqEditorPath}\" /outputToStdIO /run MainSequence \"${sequencePath}\" /quit"
 }
