8626f3fd1b47d934a982f2fa25e3d2e18d6f4525
Andy Church <church.andrew.d@gmail.com>
2018-07-21 12:14:14 -0500

    fix quote

--- vars/lvUpdateProjectFiles.groovy
+++ vars/lvUpdateProjectFiles.groovy
@@ -1,6 +1,6 @@
 def call(packageInfo, lvVersion){
 
    echo "Updating project files."
-   bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\Source\\LV\\Build\\UpdateProjectFiles.vi\"
+   bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\Source\\LV\\Build\\UpdateProjectFiles.vi\""
 
 }
