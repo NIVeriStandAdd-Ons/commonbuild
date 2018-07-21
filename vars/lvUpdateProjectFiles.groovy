f19f5744a0d9f0026ccf0de2bceccb48bf8db9c9
Andy Church <church.andrew.d@gmail.com>
2018-07-21 12:05:54 -0500

    use codegen VI from VeriStand Steps source instead of commonbuild

--- vars/lvUpdateProjectFiles.groovy
+++ vars/lvUpdateProjectFiles.groovy
@@ -1,10 +1,6 @@
 def call(packageInfo, lvVersion){
+
    echo "Updating project files."
-
-   def csProjectPath = "\\Source\\CSharp\\VeristandStepsInstaller\\OpenWorkspaceDialog\\VeristandSteps.csproj"
-   def typesVersion = packageInfo.get('types_version')
-   def tsVersions = packageInfo.get('teststand_versions')
-
-   bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\commonbuild\\lv\\codegen\\lvUpdateProjectFiles.vi\" -- \"$WORKSPACE\" \"$csProjectPath\" \"$typesVersion\" \"$tsVersions\""
+   bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\Source\\LV\\Build\\UpdateProjectFiles.vi\"
 
 }
