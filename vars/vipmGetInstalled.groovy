aaf9e670c28fa7539bc81ec6c1212ab6134ce38f
Andy Church <church.andrew.d@gmail.com>
2018-08-11 17:05:36 -0500

    work around VIPM bug with error handling

--- vars/vipmGetInstalled.groovy
+++ vars/vipmGetInstalled.groovy
@@ -1,4 +1,4 @@
 def call(lvVersion){
    echo "Getting list of installed VI Packages."
-   bat "labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\commonbuild\\lv\\vipm\\vipmGetInstalled.vi\" -- \"$WORKSPACE\""
+   bat "(labview-cli --kill --lv-ver $lvVersion \"$WORKSPACE\\commonbuild\\lv\\vipm\\vipmGetInstalled.vi\" -- \"$WORKSPACE\") || ((echo "Failed to get list of installed VI Packages. Continuing.") && (SET ERRORLEVEL=0))"
 }
