901b8dc0d232d159ae4f20f7d0d4d580f3f2522a
Michael Bilyk <33259264+mbilyk@users.noreply.github.com>
2019-07-17 12:19:33 -0500

    Copied copyProjectConfig from ni build tools
    https://github.com/ni/niveristand-custom-device-build-tools/commit/44b42748fdd9bdcf718d7c29ed3e98bbc37afeb1

--- vars/copyProjectConfig.groovy
+++ vars/copyProjectConfig.groovy
@@ -2,17 +2,17 @@
 // and versions, then replaces the versions based on the lvVersion
 def call(projectPath, lvVersion){
    echo "Copying configuration file for $projectPath"
-   def configFileName = "$projectPath" + ".config"
+   configFileName = "$projectPath" + ".config"
    
-   def currentVersion = lvVersion as int
-   def newAssemblyVersion = "${currentVersion}.0.0.0"
-   
-   def previousVersion = currentVersion - 1
-   def oldAssemblyVersion = "0.0.0.0-${previousVersion}.9.9.9"
-   
-   def fileContent = readFile "commonbuild/resources/LabVIEW.exe.config"
-   fileContent = fileContent.replaceAll("(oldVersion=\")[^\"]+","\$1$oldAssemblyVersion")
-      .replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
-   
+   def defaultVersion = ["$lvVersion": "${lvVersion}.0.0.0"]
+   def assemblyVersions = readProperties defaults: defaultVersion, file: "niveristand-custom-device-build-tools/resources/assemblyVersions.properties"
+
+   def newAssemblyVersion = assemblyVersions."$lvVersion"
+
+   def fileContent = readFile "niveristand-custom-device-build-tools/resources/LabVIEW.exe.config"
+
+   //https://regex101.com/r/tBNE56/2
+   fileContent = fileContent.replaceAll("(newVersion=\")[^\"]+","\$1$newAssemblyVersion")
+
    writeFile file: configFileName, text: fileContent
 }
